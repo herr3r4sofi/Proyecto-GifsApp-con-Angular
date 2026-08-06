bf5c315494790ac280a408b9482b5421fa860819
herr3r4sofi <shc290108@gmail.com>
2026-08-06 10:48:17 -0600

    Mostrar imagenes de relleno

--- src/app/gifs/pages/trending-page/trending-page.ts
+++ src/app/gifs/pages/trending-page/trending-page.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { GifList } from "../../components/gif-list/gif-list";
+import { GifListItem } from "../../components/gif-list/gif-list-item/gif-list-item";
 
 @Component({
   selector: 'app-trending-page',
-  imports: [],
+  imports: [GifList, GifListItem],
   templateUrl: './trending-page.html',
 })
 export default class TrendingPage {}
