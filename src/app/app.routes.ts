14186a59c99493283afd2a60b328dad0802a032e
herr3r4sofi <shc290108@gmail.com>
2026-08-12 13:40:58 -0600

    Argumentos dinámicos por URL

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,35 +2,38 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
 
-{
-  path: "dashboard",
-  loadComponent: () => import ("./gifs/pages/dashboard-page/dashboard-page"),
-  children: [
-    {
-  path: "trending",
-  loadComponent: () => import ("./gifs/pages/trending-page/trending-page"),
-},
+  {
+    path: "dashboard",
+    loadComponent: () => import("./gifs/pages/dashboard-page/dashboard-page"),
+    children: [
+      {
+        path: "trending",
+        loadComponent: () => import("./gifs/pages/trending-page/trending-page"),
+      },
 
-{
-  path: "search",
-  loadComponent: () => import ("./gifs/pages/search-page/search-page"),
-},
+      {
+        path: "search",
+        loadComponent: () => import("./gifs/pages/search-page/search-page"),
+      },
 
-{
-  path: "**",
-  redirectTo: "trending",
-}
+      {
+        path: "history/:query",
+        loadComponent: () => import("./gifs/pages/gif-history/gif-history"),
+      },
 
-  ]
-},
+      {
+        path: "**",
+        redirectTo: "trending",
+      },
+
+    ]
+  },
 
 
-
-
-{
-  path: "**",
-  redirectTo: "dashboard",
-}
+  {
+    path: "**",
+    redirectTo: "dashboard",
+  }
 
 
 ];
---
+++ src/app/gifs/pages/gif-history/gif-history.ts
@@ -0,0 +1,15 @@
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { map } from 'rxjs';
+
+@Component({
+  selector: 'app-gif-history',
+  imports: [],
+  templateUrl: './gif-history.html',
+})
+export default class GifHistory {
+  query = toSignal(
+  inject(ActivatedRoute).params.pipe(map((params) => params['query']))
+  ) 
+}
