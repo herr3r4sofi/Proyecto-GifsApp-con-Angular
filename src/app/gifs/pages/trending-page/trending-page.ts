98406584c8c2c4d637ce26f2e12132dfa30c36bc
herr3r4sofi <shc290108@gmail.com>
2026-08-25 15:24:23 -0600

    Determinar fin de scroll

--- src/app/gifs/pages/trending-page/trending-page.ts
+++ src/app/gifs/pages/trending-page/trending-page.ts
@@ -12,15 +12,23 @@ import { GifsService } from "../../services/gifs.service";
   templateUrl: './trending-page.html',
 })
 export default class TrendingPageComponent {
-  
+
   gifService = inject(GifsService);
 
   scrollDivRef = viewChild<ElementRef>('groupDiv');
 
 
+
   onScroll(event: Event) {
     const scrollDiv = this.scrollDivRef()?.nativeElement;
+    if (!scrollDiv) return;
 
-    console.log(scrollDiv);
+    const scrollTop = scrollDiv.scrollTop;
+    const clientHeight = scrollDiv.clientHeight;
+    const scrollHeight = scrollDiv.scrollHeight;
+
+    //console.log({ scrollTotal: scrollTop + clientHeight, scrollHeight });
+    const isAtBottom = scrollTop + clientHeight + 300 >= scrollHeight; 
+    console.log({ isAtBottom });
   }
 }
